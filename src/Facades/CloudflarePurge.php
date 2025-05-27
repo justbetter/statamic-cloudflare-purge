635a4acea8f14cf07ded73cb49179e1944301ded
Jade Geels <jade@justbetter.nl>
2025-05-27 11:34:38 +0200

    Fix defaults for zones

--- src/Facades/CloudflarePurge.php
+++ src/Facades/CloudflarePurge.php
@@ -5,13 +5,13 @@ namespace JustBetter\StatamicCloudflarePurge\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static void appendInvalidateUrl(string $url, ?string $zone)
- * @method static array getInvalidateUrls(?string $zone)
- * @method static array popInvalidateUrls(?string $zone)
- * @method static void clearInvalidateUrls(?string $zone)
- * @method static string getStoragePath(?string $zone)
- * @method static void appendZone(?string $zone)
- * @method static void removeZone(?string $zone)
+ * @method static void appendInvalidateUrl(string $url, ?string $zone = null)
+ * @method static array getInvalidateUrls(?string $zone = null)
+ * @method static array popInvalidateUrls(?string $zone = null)
+ * @method static void clearInvalidateUrls(?string $zone = null)
+ * @method static string getStoragePath(?string $zone = null)
+ * @method static void appendZone(?string $zone = null)
+ * @method static void removeZone(?string $zone = null)
  * @method static array getZones()
  * @method static string getCurrentZone()
  *
