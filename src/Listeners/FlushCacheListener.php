6b56e25bf62318c4dfcd24dc869f64256674eae2
Jade Geels <jade@justbetter.nl>
2025-05-27 13:29:21 +0200

    Add zone when flushing cache

--- src/Listeners/FlushCacheListener.php
+++ src/Listeners/FlushCacheListener.php
@@ -2,12 +2,15 @@
 
 namespace JustBetter\StatamicCloudflarePurge\Listeners;
 
+use JustBetter\StatamicCloudflarePurge\Facades\CloudflarePurge;
 use JustBetter\StatamicCloudflarePurge\Jobs\PurgeCloudflareCachesJob;
 
 class FlushCacheListener
 {
     public function handle($event): void
     {
+        CloudflarePurge::appendZone();
+        
         PurgeCloudflareCachesJob::dispatch(true);
     }
 }
